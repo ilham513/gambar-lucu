f89633dcd55c61e7a5bfb18dfa7bc1d4fe0f32a2
ilham513 <ilham513@outlook.com>
2020-08-23 21:25:20 +0700

    baru bootstrap

--- index.php
+++ index.php
@@ -6,9 +6,33 @@
     <link href="style.css" rel="stylesheet">
     <script src="index.js" defer></script>
     <link rel="manifest" href="manifest.webmanifest">
+	<!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.0/examples/navbar-fixed/navbar-top-fixed.css" rel="stylesheet">
   </head>
-  <body>
-    <img src="images/fox1.jpg" alt="a fox picture">
-    <button class="add-button">Add to home screen</button>
-  </body>
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">Gambar Lucu Heroku</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main role="main" class="container">
+      <div class="jumbotron">
+        <h1>Install Mini Aplikasi Dulu!</h1>
+        <p class="lead">Tinggal Klik Tombol Install > Cek Homescreen Kamu.</p>
+		<button class="add-button btn btn-lg btn-primary">Install Ke Home Screen</button>
+      </div>
+    </main>
+
+	</body>
 </html>
